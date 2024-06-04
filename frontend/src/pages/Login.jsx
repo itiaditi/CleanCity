b7796fbd9c8a589f2049e53442ebaa186795a517
itiaditi <aditi.tlgt244@gmail.com>
2024-06-04 23:28:35 +0530

    login page design is done

---
+++ frontend/src/pages/Login.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const Login = () => {
+  return (
+    <>
+   <div className='login-bg-image w-full bg-black'>
+   <div className='container'>
+        <div className='w-full'>
+          
+          <form class="login">
+  <input type="text" placeholder="Username"/>
+  <input type="password" placeholder="Password"/>
+  <button>Login</button>
+</form>
+
+          
+        </div>
+    </div>
+   </div>
+    </>
+  )
+}
+
+export default Login
